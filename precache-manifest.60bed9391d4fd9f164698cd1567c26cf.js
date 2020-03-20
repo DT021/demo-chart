1eb7a86e018a66656e7d91e33c45fe734026e28e
Ekaterina Sosinovich <ekaterina.sosinovich@duallab.com>
2020-03-20 11:14:47 +0300

    Updates

---
+++ precache-manifest.60bed9391d4fd9f164698cd1567c26cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1383434873d187f1d399f721706c7da",
+    "url": "/demo-chart/index.html"
+  },
+  {
+    "revision": "fb76e52e0c18c2f12f9c",
+    "url": "/demo-chart/static/css/main.10484fae.chunk.css"
+  },
+  {
+    "revision": "33defb2f02eb3e98d984",
+    "url": "/demo-chart/static/js/2.647de234.chunk.js"
+  },
+  {
+    "revision": "a4cb1f7460f303f3662c122645817fc8",
+    "url": "/demo-chart/static/js/2.647de234.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb76e52e0c18c2f12f9c",
+    "url": "/demo-chart/static/js/main.a6c1c25e.chunk.js"
+  },
+  {
+    "revision": "3b62764a89f7dfb30bae",
+    "url": "/demo-chart/static/js/runtime-main.460b356d.js"
+  }
+]);
